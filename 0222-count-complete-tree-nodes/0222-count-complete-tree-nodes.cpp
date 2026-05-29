096001fc0cc8521bad76f00d0740b9300276bded
Krish Gupta <guptakrish490@gmail.com>
2026-05-29 15:27:19 +0530

    Time: 0 ms (100%), Space: 31.4 MB (17.42%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -15,25 +15,6 @@ public:
 
         if(!root) return 0;
 
-        queue<TreeNode*> q;
-        q.push(root);
-        vector<int> ans;
-
-        while(!q.empty()){
-            int size=q.size();
-            
-            for(int i=0;i<size;i++){
-
-                TreeNode* node=q.front(); q.pop();
-
-                ans.push_back(node->val);
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-
-            }
-            
-        }
-
-        return ans.size();
+        return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
