150a57c6ddcfa30b9164fd452f8b36e652f47011
Krish Gupta <guptakrish490@gmail.com>
2026-05-30 12:37:26 +0530

    Time: 0 ms (100%), Space: 12.5 MB (75.96%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -20,7 +20,7 @@ public:
         if(!left) root->left=nullptr;
         if(!right) root->right=nullptr;
 
-        return root->val || left || right;
+        return root->val==1 || left || right;
     }
 
     TreeNode* pruneTree(TreeNode* root) {
