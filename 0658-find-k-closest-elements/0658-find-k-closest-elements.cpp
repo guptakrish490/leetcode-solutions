ba4a975ffcdebaed09f9b602ce2178239038fb18
Krish Gupta <guptakrish490@gmail.com>
2026-06-02 12:34:16 +0530

    Time: 35 ms (9.51%), Space: 38.8 MB (12.66%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -5,17 +5,20 @@ public:
 
         struct Compare{
             bool operator()(const pair<int,int> &a,const pair<int,int> &b){
-                if(a.second==b.second) return a.first>b.first;
-                return a.second>b.second;
+                if(a.second==b.second) return a.first<b.first;
+                return a.second<b.second;
             }
         };
 
         priority_queue<pair<int,int>, vector<pair<int,int>>, Compare> pq;
         vector<int> ans;
 
-        for(int num:arr) pq.push({num , abs(num-x)});
+        for(int num:arr) {
+            pq.push({num , abs(num-x)});
+            if(pq.size()>k) pq.pop();
+        }
 
-        for(int i=0;i<k;i++){
+        while(!pq.empty()){
             ans.push_back(pq.top().first);
             pq.pop();
         }
