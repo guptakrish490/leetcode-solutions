ddf11882785e1687c331d95235c556d99a2efa76
Krish Gupta <guptakrish490@gmail.com>
2026-06-03 12:52:33 +0530

    Time: 108 ms (20.26%), Space: 131.5 MB (29.26%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        
-        struct Compare {
-            bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
-                return a.second < b.second;
-            }
-        };
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare>pq;
-        vector<pair<int,int>> temp;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minCapital;
+        priority_queue<int> maxProfit;
 
         for(int i=0;i<profits.size();i++){
-            pq.push({capital[i],profits[i]});
+            minCapital.push({capital[i],profits[i]});
         }
 
-        for(int i=0;i<profits.size();i++){
-            if(k==0) break;
-
-            while(!pq.empty() && pq.top().first > w){
-                temp.push_back(pq.top());
-                pq.pop();
+        for(int i=0;i<k;i++){
+            while(!minCapital.empty() && minCapital.top().first<=w){
+                maxProfit.push(minCapital.top().second);
+                minCapital.pop();
             }
 
-            if(!pq.empty()){
-                w+=pq.top().second;
-                k--;
-                pq.pop();
-            }
-            else return w;
+            if(maxProfit.empty()) break;
 
-            for(pair<int,int>& num:temp) pq.push(num);
-            temp.clear();
+            w+=maxProfit.top();
+            maxProfit.pop();
         }
 
         return w;
