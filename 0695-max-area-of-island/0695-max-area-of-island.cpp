a0f5dcf2c0b061f8d2c2eebae4e1b2e151576c3d
Krish Gupta <guptakrish490@gmail.com>
2026-06-07 16:23:18 +0530

    Time: 0 ms (100%), Space: 28.7 MB (35.58%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<int>>& vis, vector<vector<int>>& grid, int& area){
-        int m=grid.size(); int n=grid[0].size();
+    void dfs(int r, int c, vector<vector<int>>& vis, vector<vector<int>>& grid,
+             int& area) {
+        int m = grid.size();
+        int n = grid[0].size();
 
-        vis[r][c]=1;
+        vis[r][c] = 1;
 
-        int dr[4]={1,-1,0,0};
-        int dc[4]={0,0,1,-1};
+        int dr[4] = {1, -1, 0, 0};
+        int dc[4] = {0, 0, 1, -1};
 
-        for(int k=0;k<4;k++){
-            int nr=r+dr[k];
-            int nc=c+dc[k];
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
 
-            if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && grid[nr][nc]){
+            if (nr >= 0 && nr < m && nc >= 0 && nc < n && !vis[nr][nc] &&
+                grid[nr][nc]) {
                 area++;
-                dfs(nr,nc,vis,grid,area);
+                dfs(nr, nc, vis, grid, area);
             }
         }
     }
+    
+
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size(); int n=grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        int maxArea=0;
+        int maxArea = 0;
 
-        vector<vector<int>>vis(m,vector<int>(n,0));
+        vector<vector<int>> vis(m, vector<int>(n, 0));
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-                if(grid[i][j] && !vis[i][j]){
-                    int area=1;
-                    
-                    dfs(i,j,vis,grid,area);
-                    maxArea=max(maxArea,area);
+                if (grid[i][j] && !vis[i][j]) {
+                    int area = 1;
+
+                    dfs(i, j, vis, grid, area);
+                    maxArea = max(maxArea, area);
                 }
             }
         }
