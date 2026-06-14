d315e9789d615ee66c488cea69b0fc361c240b9a
Krish Gupta <guptakrish490@gmail.com>
2026-06-14 12:03:57 +0530

    Time: 8 ms (34.3%), Space: 20.2 MB (24.72%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
-        vis[node] = 1;
-
-        for (auto it : adj[node]) {
-            if (vis[it]==1) {
-                return false;
-            } else if (!vis[it]) {
-                if (!dfs(it, adj, vis))
-                    return false;
-            }
-        }
-        vis[node]=2;
-
-        return true;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses, 0);
-        vector<vector<int>> adj(numCourses);
+        int V=numCourses;
+        int cnt=0;
+        vector<vector<int>>adj(V);
+        vector<int> indegree(V,0);
+        queue<int>q;
 
-        for (int i = 0; i < prerequisites.size(); i++) {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        for(auto it:prerequisites){
+            adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
         }
 
-        for (int i = 0; i < adj.size(); i++) {
-            if (!vis[i]) {
-                if (!dfs(i, adj, vis))
-                    return false;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+
+        while(!q.empty()){
+            int node=q.front(); q.pop();
+            cnt++;
+
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(!indegree[it]) q.push(it);
             }
         }
 
-        return true;
+
+        return cnt==V;
     }
 };
