000eb2d8dffd3d9ccbd036690c8e72cc1c094cbe
Krish Gupta <guptakrish490@gmail.com>
2026-06-25 22:27:26 +0530

    Time: 5 ms (30.25%), Space: 28.6 MB (43.3%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<int>>& vis, vector<vector<int>>& grid,
-             int& area) {
-        int m = grid.size();
-        int n = grid[0].size();
+    void traverse(int r, int c, vector<vector<int>>&vis, vector<vector<int>>& grid, int& area){
+        int m=grid.size();
+        int n=grid[0].size();
 
-        vis[r][c] = 1;
+        vis[r][c]=1;
+        area++;
 
-        int dr[4] = {1, -1, 0, 0};
-        int dc[4] = {0, 0, 1, -1};
+        int dr[4]={-1,1,0,0};
+        int dc[4]={0,0,1,-1};
 
-        for (int k = 0; k < 4; k++) {
-            int nr = r + dr[k];
-            int nc = c + dc[k];
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
 
-            if (nr >= 0 && nr < m && nc >= 0 && nc < n && !vis[nr][nc] &&
-                grid[nr][nc]) {
-                area++;
-                dfs(nr, nc, vis, grid, area);
+            if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc] && !vis[nr][nc]){
+                traverse(nr,nc,vis,grid,area);
             }
         }
     }
-    
+
 
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
 
-        int maxArea = 0;
+        vector<vector<int>>vis(m,vector<int>(n,0));
 
-        vector<vector<int>> vis(m, vector<int>(n, 0));
+        int maxArea=0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
 
-                if (grid[i][j] && !vis[i][j]) {
-                    int area = 1;
-
-                    dfs(i, j, vis, grid, area);
-                    maxArea = max(maxArea, area);
+                if(grid[i][j] && !vis[i][j]){
+                    int area=0;
+                    traverse(i,j,vis,grid,area);
+                    maxArea=max(area,maxArea);
                 }
             }
         }
