1154a7c14d9848752feacedd11d3d3c3835559db
Krish Gupta <guptakrish490@gmail.com>
2026-07-06 11:06:24 +0530

    Time: 52 ms (73.28%), Space: 21.2 MB (48.2%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord,
-                     vector<string>& wordList) {
-        queue<pair<string, int>> q;
-        unordered_set<string> st(wordList.begin(), wordList.end());
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string>st(wordList.begin(),wordList.end());
+        queue<pair<string,int>>q;
 
-        q.push({beginWord, 1});
-
-        while (!q.empty()) {
-            string word = q.front().first;
-            int dist = q.front().second;
-
+        q.push({beginWord,1});
+        while(!q.empty()){
+            string curr=q.front().first;
+            int dist=q.front().second;
             q.pop();
 
-            if (word == endWord)
-                return dist;
+            if(curr==endWord) return dist;
 
-            for (int i = 0; i < word.size(); i++) {
-                string pattern = word;
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    pattern[i] = ch;
+            for(int i=0;i<curr.size();i++){
+                string pattern=curr;
+                for(char ch='a';ch<='z';ch++){
+                    pattern[i]=ch;
 
-                    if (st.find(pattern) != st.end()) {
+                    if(st.find(pattern)!=st.end()){
                         st.erase(pattern);
-                        q.push({pattern, dist + 1});
+                        q.push({pattern,dist+1});
                     }
                 }
             }
