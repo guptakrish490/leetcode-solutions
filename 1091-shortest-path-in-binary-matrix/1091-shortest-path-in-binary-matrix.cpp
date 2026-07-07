003fefca83569197ca4abc15e076ba7c1102dcf1
Krish Gupta <guptakrish490@gmail.com>
2026-07-07 13:09:48 +0530

    Time: 29 ms (29.76%), Space: 23.4 MB (92.56%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    int bfs(int r, int c, vector<vector<int>>& grid, int n) {
-        vector<vector<int>> dist(n, (vector<int>(n, -1)));
-        queue<pair<int, int>> q;
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int V = grid.size();
+        if (grid[0][0] || grid[V - 1][V - 1])
+            return -1;
 
-        int dr[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
-        int dc[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
+        vector<vector<int>> dist(V, vector<int>(V, 1e9));
 
-        dist[r][c] = 1;
-        q.push({r, c});
+        dist[0][0] = 1;
+        pq.push({1, {0, 0}});
 
-        while (!q.empty()) {
-            auto [row, col] = q.front();
-            q.pop();
+        while (!pq.empty()) {
+            auto top = pq.top();
+            int dis = top.first;
+            int r = top.second.first;
+            int c = top.second.second;
+            pq.pop();
 
-            if (row == n - 1 && col == n - 1)
-                return dist[row][col];
+            if (r == V - 1 && c == V - 1)
+                return dis;
+
+            int dr[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+            int dc[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
             for (int i = 0; i < 8; i++) {
-                int nr = row + dr[i];
-                int nc = col + dc[i];
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !grid[nr][nc] &&
-                    dist[nr][nc] == -1) {
-                    dist[nr][nc] = dist[row][col] + 1;
-                    q.push({nr, nc});
+                if (nr >= 0 && nr < V && nc >= 0 && nc < V && !grid[nr][nc]) {
+                    if (dist[nr][nc] > dis + 1) {
+                        dist[nr][nc] = dis + 1;
+                        pq.push({dist[nr][nc], {nr, nc}});
+                    }
                 }
             }
         }
 
-        return -1;
-    }
-
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
-            return -1;
-
-        int ans = bfs(0, 0, grid, n);
-
-        return ans;
+        return (dist[V - 1][V - 1] == 1e9) ? -1 : dist[V - 1][V - 1];
     }
 };
