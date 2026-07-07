8981954e67cd97ac9d4bfd34534a53957bbe247e
Krish Gupta <guptakrish490@gmail.com>
2026-07-07 13:14:40 +0530

    Time: 8 ms (94.95%), Space: 25 MB (63.54%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -5,36 +5,30 @@ public:
         if (grid[0][0] || grid[V - 1][V - 1])
             return -1;
 
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>>
-            pq;
+        queue<pair<int,int>>q;
         vector<vector<int>> dist(V, vector<int>(V, 1e9));
 
         dist[0][0] = 1;
-        pq.push({1, {0, 0}});
+        q.push({0, 0});
 
-        while (!pq.empty()) {
-            auto top = pq.top();
-            int dis = top.first;
-            int r = top.second.first;
-            int c = top.second.second;
-            pq.pop();
+        int dr[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dc[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        while (!q.empty()) {
+            auto [r,c]=q.front();
+            q.pop();
 
             if (r == V - 1 && c == V - 1)
-                return dis;
-
-            int dr[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-            int dc[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+                return dist[r][c];
 
             for (int i = 0; i < 8; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
 
                 if (nr >= 0 && nr < V && nc >= 0 && nc < V && !grid[nr][nc]) {
-                    if (dist[nr][nc] > dis + 1) {
-                        dist[nr][nc] = dis + 1;
-                        pq.push({dist[nr][nc], {nr, nc}});
+                    if (dist[nr][nc] > dist[r][c] + 1) {
+                        dist[nr][nc] = dist[r][c] + 1;
+                        q.push({nr, nc});
                     }
                 }
             }
