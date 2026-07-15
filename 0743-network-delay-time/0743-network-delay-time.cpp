05f77dfdbbea3ac64b6a58f4fd40d153a85a04fb
Krish Gupta <guptakrish490@gmail.com>
2026-07-15 12:34:49 +0530

    Time: 73 ms (90.15%), Space: 44.4 MB (53.24%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -2,39 +2,42 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
 
-        set<pair<int, int>> st;
-        vector<vector<pair<int, int>>> adj(n+1);
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        vector<vector<pair<int, int>>> adj(n + 1);
 
         for (int i = 0; i < times.size(); i++) {
             adj[times[i][0]].push_back({times[i][1], times[i][2]});
         }
 
         int minTime = 0;
-        vector<int> dist(n+1, 1e9);
+        vector<int> dist(n + 1, 1e9);
 
         dist[k] = 0;
-        st.insert({0, k});
+        pq.push({0, k});
 
-        while (!st.empty()) {
-            auto [dis, node] = *(st.begin());
-            st.erase(st.begin());
+        while (!pq.empty()) {
+            auto [dis, node] = pq.top();
+            pq.pop();
+
+            if (dis > dist[node])
+                continue;
 
             for (auto it : adj[node]) {
                 int adjNode = it.first;
                 int edgeWeight = it.second;
 
                 if (dis + edgeWeight < dist[adjNode]) {
-                    if (dist[adjNode] != 1e9) {
-                        st.erase({dist[adjNode], adjNode});
-                    }
                     dist[adjNode] = dis + edgeWeight;
-                    st.insert({dist[adjNode], adjNode});
+                    pq.push({dist[adjNode], adjNode});
                 }
             }
         }
 
-        for (int i=1;i<dist.size();i++) {
-            if (dist[i] == 1e9) return -1;
+        for (int i = 1; i < dist.size(); i++) {
+            if (dist[i] == 1e9)
+                return -1;
             minTime = max(minTime, dist[i]);
         }
 
