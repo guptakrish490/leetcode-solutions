4e52a9d869871769fab04f777dbd0b9e9cd39a45
Krish Gupta <guptakrish490@gmail.com>
2026-07-16 11:46:06 +0530

    Time: 0 ms (100%), Space: 19.2 MB (16.56%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> adj(n);
-        for (auto &f : flights) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+                          int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& f : flights) {
             adj[f[0]].push_back({f[1], f[2]});
         }
 
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
+                       greater<>>
+            pq;
+        vector<vector<int>> dist(n, (vector<int>(k + 2, 1e9)));
 
-        vector<vector<int>> dist(n, vector<int>(k+2, INT_MAX));
         dist[src][0] = 0;
-
-        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> pq;
         pq.push({0, src, 0});
 
         while (!pq.empty()) {
-            auto [cost, node, stops] = pq.top();
+            auto [cost, node, stop] = pq.top();
             pq.pop();
 
-            if (node == dst) return cost;
-            if (stops == k+1) continue;
+            if (node == dst)
+                return cost;
+            if (stop > k)
+                continue;
 
-            for (auto &edge : adj[node]) {
-                int next = edge.first;
-                int price = edge.second;
+            for (auto it : adj[node]) {
+                auto [next, price] = it;
 
-                if (cost + price < dist[next][stops+1]) {
-                    dist[next][stops+1] = cost + price;
-                    pq.push({dist[next][stops+1], next, stops+1});
+                if (cost + price < dist[next][stop + 1]) {
+                    dist[next][stop + 1] = cost + price;
+                    pq.push({dist[next][stop + 1], next, stop + 1});
                 }
             }
         }
