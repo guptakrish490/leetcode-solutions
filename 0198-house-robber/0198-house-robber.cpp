8df24aede1b0def14b9c946a5459494ebee3f18b
Krish Gupta <guptakrish490@gmail.com>
2026-09-15 14:16:43 +0530

    Time: 0 ms (100%), Space: 10.1 MB (79.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,14 +7,16 @@ public:
         if (n == 1)
             return nums[0];
 
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int prev1, prev2;
+        prev2 = nums[0];
+        prev1 = max(nums[0], nums[1]);
 
         for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+            int curr = max(prev1, prev2 + nums[i]);
+            prev2=prev1;
+            prev1=curr;
         }
 
-        return dp[n - 1];
+        return prev1;
     }
 };
