57e8281ca2e55bba534bc0a8f9b920b21d253e51
Krish Gupta <guptakrish490@gmail.com>
2026-09-19 19:11:16 +0530

    Time: 0 ms (100%), Space: 15.6 MB (85.6%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -3,19 +3,20 @@ public:
     int deleteAndEarn(vector<int>& nums) {
         int sz = *max_element(nums.begin(), nums.end());
         vector<int> scores(sz + 1, 0);
-        vector<int> dp(sz + 1, -1);
 
         for (int i = 0; i < nums.size(); i++) {
             scores[nums[i]]++;
         }
 
-        dp[0] = 0;
-        dp[1] = scores[1];
+        int prev2 = 0;
+        int prev = scores[1];
 
         for (int i = 2; i <= sz; i++) {
-            dp[i] = max(scores[i] * i + dp[i - 2], dp[i - 1]);
+            int curr = max(scores[i] * i + prev2, prev);
+            prev2=prev;
+            prev=curr;
         }
 
-        return dp[sz];
+        return prev;
     }
 };
