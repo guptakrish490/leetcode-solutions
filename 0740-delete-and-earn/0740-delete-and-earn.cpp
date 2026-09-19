838189e81dce16704f5a31381f783b6533fb88e6
Krish Gupta <guptakrish490@gmail.com>
2026-09-19 19:10:07 +0530

    Time: 0 ms (100%), Space: 15.8 MB (75.03%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int f(int idx, vector<int>& scores, vector<int>& dp) {
-        if (idx <= 0)
-            return 0;
-
-        if (dp[idx] != -1)
-            return dp[idx];
-
-        int pick = scores[idx] * idx + f(idx - 2, scores, dp);
-        int notPick = f(idx - 1, scores, dp);
-
-        return dp[idx] = max(pick, notPick);
-    }
-
     int deleteAndEarn(vector<int>& nums) {
         int sz = *max_element(nums.begin(), nums.end());
-        vector<int> scores(sz + 1);
+        vector<int> scores(sz + 1, 0);
         vector<int> dp(sz + 1, -1);
 
         for (int i = 0; i < nums.size(); i++) {
             scores[nums[i]]++;
         }
 
-        return f(sz, scores, dp);
+        dp[0] = 0;
+        dp[1] = scores[1];
+
+        for (int i = 2; i <= sz; i++) {
+            dp[i] = max(scores[i] * i + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[sz];
     }
 };
