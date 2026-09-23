8f7f4af64c2ec2e8c2d01e65d21df2730dd72e3c
Krish Gupta <guptakrish490@gmail.com>
2026-09-23 19:38:55 +0530

    Time: 0 ms (100%), Space: 21.4 MB (70.07%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -25,39 +25,42 @@ public:
 
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<int> prev(n, 0);
 
         for (int i = 0; i < n; i++) {
-            dp[0][i] = grid[0][i];
+            prev[i] = grid[0][i];
         }
 
         int minPathSum = 1e9;
 
         for (int i = 1; i < n; i++) {
+            vector<int> curr(n);
             int min1 = INT_MAX, min2 = INT_MAX, minIdx = -1;
 
             for (int j = 0; j < n; j++) {
-                if (dp[i - 1][j] < min1) {
+                if (prev[j] < min1) {
                     min2 = min1;
-                    min1 = dp[i - 1][j];
+                    min1 = prev[j];
                     minIdx = j;
-                } else if (dp[i - 1][j] < min2) {
-                    min2 = dp[i - 1][j];
+                } else if (prev[j] < min2) {
+                    min2 = prev[j];
                 }
             }
 
             for (int j = 0; j < n; j++) {
                 if (minIdx == j)
-                    dp[i][j] = min2;
+                    curr[j] = min2;
                 else
-                    dp[i][j] = min1;
+                    curr[j] = min1;
 
-                dp[i][j] += grid[i][j];
+                curr[j] += grid[i][j];
             }
+
+            prev = curr;
         }
 
         for (int i = 0; i < n; i++) {
-            minPathSum = min(minPathSum, dp[n - 1][i]);
+            minPathSum = min(minPathSum, prev[i]);
         }
 
         return minPathSum;
